a0662cc0b56eb4b2d346136187a23c22a83a90f7
Henry Wong <joe8632782@gmail.com>
2020-02-11 13:40:50 +0800

    Initialize sequelize and connect to database

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ const express = require("express")
 const app = express()
 const port = 3000
 const exhbs = require("express-handlebars")
+const db = require("./models")
 
 app.engine("handlebars", exhbs({ defaultLayout: "main" })) // Handlebars 註冊樣板引擎
 app.set("view engine", "handlebars") // 設定使用 Handlebars 做為樣板引擎
 
 app.listen(port, () => {
+  db.sequelize.sync()
   console.log("App is running!!")
 })
 require("./routes")(app)
--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,9 @@ module.exports = app => {
   app.get("/", (req, res) => { res.redirect("restaurants") })
   app.get("/restaurants", restController.getRestaurants)
 
+  // 連到 /admin 頁面就轉到 /admin/restaurants
   app.get("/admin", (req, res) => { res.redirect("admin/restaurants") })
+
+  // 在 /admin/restaurants 底下則交給 adminController.getRestaurants 處理
   app.get("/admin/restaurants", adminController.getRestaurants)
 }
