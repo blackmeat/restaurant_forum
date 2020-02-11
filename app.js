af0221a9a68f7c0ded89dba968393064e11d7f29
Henry Wong <joe8632782@gmail.com>
2020-02-11 16:29:33 +0800

    Add flash feature,update userController signUp

--- app.js
+++ app.js
@@ -4,10 +4,21 @@ const port = 3000
 const exhbs = require("express-handlebars")
 const db = require("./models")
 const bodyParser = require("body-parser")
+const session = require("express-session")
+const flash = require("connect-flash")
 
 app.engine("handlebars", exhbs({ defaultLayout: "main" })) // Handlebars 註冊樣板引擎
 app.set("view engine", "handlebars") // 設定使用 Handlebars 做為樣板引擎
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({ extended: true })) //設定使用 body-parser 解析表單內容
+app.use(session({ secret: "12345", resave: false, saveUninitialized: false }))
+app.use(flash())
+
+// 把 req.flash 放到 res.locals 裡面
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success_messages')
+  res.locals.error_messages = req.flash('error_messages')
+  next()
+})
 
 app.listen(port, () => {
   db.sequelize.sync()
