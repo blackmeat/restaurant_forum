33a7d20fe474506cd747f9a3b64dfcfd8ababcbc
Henry Wong <joe8632782@gmail.com>
2020-02-12 13:14:45 +0800

    Update port

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 const exhbs = require("express-handlebars")
 const db = require("./models")
 const bodyParser = require("body-parser")
