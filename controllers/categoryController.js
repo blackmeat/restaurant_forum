7051a1ea3001bcdf916fb6a3f4f67798f41f1ec2
Henry Wong <joe8632782@gmail.com>
2020-02-17 23:06:46 +0800

    Add admin categories delete feature

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -31,6 +31,7 @@ const categoryController = {
       res.redirect("/admin/categories")
     })
   },
+
   putCategory: (req, res) => {
     if (!req.body.name) {
       req.flash("error_messages", "The name didn\'t exist!!")
@@ -42,11 +43,23 @@ const categoryController = {
         return category.update({
           name: req.body.name
         }).then((category) => {
-          req.flash("success_messages", "The new category was successful updated!!")
+          req.flash("success_messages", "The new category was updated successful!!")
           res.redirect("/admin/categories")
         })
       })
+  },
 
+  deleteCategory: (req, res) => {
+    return Category
+      .findByPk(req.params.id)
+      .then((category) => {
+        category
+          .destroy()
+          .then((category) => {
+            req.flash("success_messages", "The category was deleted successful")
+            res.redirect('/admin/categories')
+          })
+      })
   }
 }
 
