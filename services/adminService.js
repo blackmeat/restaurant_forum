5703aef14debebe4f96babbea7bdc2c71354bd94
Henry Wong <joe8632782@gmail.com>
2020-02-25 12:09:46 +0800

    PUT /api/admin/restaurants/:id

--- services/adminService.js
+++ services/adminService.js
@@ -64,6 +64,56 @@ const adminController = {
     }
   },
 
+  putRestaurant: (req, res, callback) => {
+    if (!req.body.name) {
+      callback({ status: "error", message: "The restaurant doesn't exist!!" })
+      // req.flash("error_messages", "餐廳名稱不存在")
+      // res.redirect("back")
+    }
+    const { file } = req
+    if (file) {
+      imgur.setClientID(IMGUR_CLIENT_ID);
+      imgur.upload(file.path, (err, img) => {
+        return Restaurant
+          .findByPk(req.params.id)
+          .then((restaurant) => {
+            restaurant.update({
+              name: req.body.name,
+              tel: req.body.tel,
+              address: req.body.address,
+              opening_hours: req.body.opening_hours,
+              description: req.body.description,
+              image: file ? img.data.link : restaurant.image,
+              CategoryId: req.body.categoryId
+            })
+              .then((restaurant) => {
+                callback({ status: "success", message: "The restaurant was updated successfully" })
+                // req.flash('success_messages', 'restaurant was successfully to update')
+                // res.redirect('/admin/restaurants')
+              })
+          })
+      })
+    } else {
+      return Restaurant
+        .findByPk(req.params.id)
+        .then((Restaurant) => {
+          Restaurant.update({
+            name: req.body.name,
+            address: req.body.address,
+            tel: req.body.tel,
+            opening_hours: req.body.opening_hours,
+            description: req.body.description,
+            image: Restaurant.image,
+            CategoryId: req.body.categoryId
+          }).then((restaurant) => {
+            callback({ status: "success", message: "The restaurant was updated successfully" })
+            // req.flash("success_messages", "restaurant was successful update")
+            // res.redirect("/admin/restaurants")
+          })
+        })
+    }
+  },
+
   deleteRestaurant: (req, res, callback) => {
     Restaurant
       .findByPk(req.params.id)
